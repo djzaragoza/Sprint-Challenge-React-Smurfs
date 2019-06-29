4327c6e21beaf73a2b98964e5f1174a5d5a3e6b8
DJ Zaragoza <zaragoza0126@gmail.com>
2019-06-29 10:55:22 -0700

    finished smurfs.js

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
+import "./Smurf.css";
 
 import Smurf from './Smurf';
 
-class Smurfs extends Component {
-  render() {
-    return (
-      <div className="Smurfs">
-        <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+const Smurfs = props => {
+  return (
+    <div className="Smurfs">
+      <h1>Smurf Village</h1>
+      <ui>
+        {props.smurfs.map(smurf => {
+          return (
+            <Smurf
+              name={smurf.name}
+              id={smurf.id}
+              age={smurf.age}
+              height={smurf.height}
+              key={smurf.id}
+              deleteSmurf={props.deleteSmurf}
+            />
+          );
+        })}
+      </ui>
+    </div>
+  );
 }
 
 Smurf.defaultProps = {
