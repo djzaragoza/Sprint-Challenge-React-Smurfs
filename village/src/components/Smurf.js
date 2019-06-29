dae1ed8b552defaae49e3ef20c4b6c7d56cbd86f
DJ Zaragoza <zaragoza0126@gmail.com>
2019-06-29 10:14:13 -0700

    working on smurf.js file

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import "./Smurf.css"
 
-const Smurf = props => {
-  return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </div>
-  );
-};
+class Smurf extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ id: this.props.id })
+  }
+
+  deleteHelper = e => {
+    e.preventDefault();
+    this.props.deleteSmurf(this.state.id);
+  }
+
+  render() {
+    return (
+      <div className="Smurf">
+        <h3>{this.props.name}</h3>
+      </div>
+    )
+  }
+}
+
+
 
 Smurf.defaultProps = {
   name: '',
