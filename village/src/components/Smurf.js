f1a92c34300ffee0c381ac86d7cd438539bcb2aa
DJ Zaragoza <zaragoza0126@gmail.com>
2019-06-29 10:17:22 -0700

    finished smurf.js

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -22,12 +22,15 @@ class Smurf extends React.Component {
     return (
       <div className="Smurf">
         <h3>{this.props.name}</h3>
+        <strong>{this.props.height} tall </strong>
+        <p>{this.props.age} smurf years old</p>
+        <form>
+          <button type="submit" onClick={this.deleteHelper}><i className="fal fa-trash"></i></button>
+        </form>
       </div>
-    )
+    );
   }
-}
-
-
+};
 
 Smurf.defaultProps = {
   name: '',
